dfe6c2402658f00bd882713d8743f545d730cf08
Ashley Hindle <hey@ashleyhindle.com>
2015-11-19 22:03:02 +0000

    Welcome file, correct wait music

--- simple-conference.php
+++ simple-conference.php
@@ -3,7 +3,8 @@ $protocol = (isset($_SERVER['HTTPS'])) ? 'https' : 'http';
 $url = "{$protocol}://{$_SERVER['HTTP_HOST']}";
 $url .= str_replace(basename(__FILE__), '',  $_SERVER['PHP_SELF']);
 
-$waitUrl = (file_exists('sounds/wait.mp3')) ? $url . 'wait.mp3' : 'http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient';
+$waitUrl = (file_exists('sounds/wait.wav')) ? $url . 'wait.mp3' : 'http://twimlets.com/holdmusic?Bucket=com.twilio.music.ambient';
+$welcomeAction = (file_exists('sounds/welcome.wav')) ? "<Play>{$url}/sounds/welcome.wav</Play>" : '<Say>Welcome to My Conference Is On Fire!</Say>';
 $joiningAction = (file_exists('sounds/joining.wav')) ? "<Play>{$url}/sounds/joining.wav</Play>" : '<Say>Joining the conference now</Say>';
 $enterPinAction = (file_exists('sounds/enter-pin.wav')) ? "<Play>{$url}/sounds/enter-pin.wav</Play>" : '<Say>Please enter your conference pin number followed by the pound or hash key.</Say>';
 $invalidPinAction = (file_exists('sounds/invalid-pin.wav')) ? "<Play>{$url}/sounds/invalid-pin.wav</Play>" : '<Say>Joining the conference now</Say>';
@@ -20,6 +21,7 @@ TWIML;
 } else {
     echo <<<TWIML
     <Response>
+	{$welcomeAction}
         <Gather timeout="10" finishOnKey="#">
 		{$enterPinAction}
         </Gather>
