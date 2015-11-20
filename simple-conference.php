f5089a3b52471a8e01a1badd73d86a74a7ad1902
Ashley Hindle <hey@ashleyhindle.com>
2015-11-20 21:25:30 +0000

    Get correct conference name

--- simple-conference.php
+++ simple-conference.php
@@ -13,13 +13,19 @@ $sounds = [
 
 // This is my horrible voice at the minute, so only play it when we have a good one
 $joiningAction = (file_exists($sounds['joining']['local'])) ? "<Play>{$sounds['joining']['remote']}</Play>" : '';
+$conferenceCode = false;
+if (isset($_POST['Digits'])) {
+	$conferenceCode = $_POST['Digits'];
+} elseif(isset($_GET['conferenceCode'])) {
+	$conferenceCode = $_GET['conferenceCode'];
+}
 
-if (isset($_POST['Digits']) || isset($_GET['conferenceCode']) && !empty($_GET['conferenceCode'])) {
+if ($conferenceCode !== false) {
     echo <<<TWIML
     <Response>
 	{$joiningAction}
         <Dial>
-            <Conference waitMethod="GET" waitUrl="{$sounds['wait']['remote']}" beep="true">mciof{$_POST['Digits']}</Conference>
+            <Conference waitMethod="GET" waitUrl="{$sounds['wait']['remote']}" beep="true">mciof{$conferenceCode}</Conference>
         </Dial>
     </Response>
 TWIML;
